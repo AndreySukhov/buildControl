118c03f4d5e8d4a0dc77686d69fad4ef4105bc6d
AndreySukhov <andrey.sush.1990@mail.ru>
2025-09-26 02:20:09 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { ConfigProvider, Layout } from 'antd';
 import ruRU from 'antd/locale/ru_RU';
 import { Login } from './pages/Login';
