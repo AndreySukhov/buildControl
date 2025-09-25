eab1759290e20c0ea51a8e6f69d1af8115eaf5bf
AndreySukhov <andrey.sush.1990@mail.ru>
2025-09-26 02:13:00 +0300

    test

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/buildControl/', // Замените на имя вашего репозитория
+  // base: '/buildControl/', // Замените на имя вашего репозитория
   build: {
     outDir: 'dist',
   }
